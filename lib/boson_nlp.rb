667ceccc563a9b09b1574aa7155e4fb83e7b9e9c
tshunglee <tshunglee.tan@whalemove.com>
2017-04-18 17:00:25 +0800

    Update readme, add configure support and httparty dependency

--- lib/boson_nlp.rb
+++ lib/boson_nlp.rb
@@ -1,5 +1,21 @@
-require "boson_nlp/version"
+require 'boson_nlp/version'
 
 module BosonNlp
   # Your code goes here...
+
+  class Configuration
+    attr_accessor :api_token
+  end
+
+  class << self
+    attr_writer :configuration
+  end
+
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration)
+  end
 end
--- spec/boson_nlp_spec.rb
+++ spec/boson_nlp_spec.rb
@@ -1,11 +1,20 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe BosonNlp do
-  it "has a version number" do
+  it 'has a version number' do
     expect(BosonNlp::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'configure' do
+    before do
+      BosonNlp.configure do |config|
+        config.api_token = 'test_token'
+      end
+    end
+
+    it 'get token success' do
+      configure = BosonNlp.configuration
+      expect(configure.api_token).to eq 'test_token'
+    end
   end
 end
